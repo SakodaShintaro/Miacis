4999c62be3aa851937f453c0fe9cd8637bcd37fe
Shintaro Sakoda <rgbygscrsedppbwg@gmial.com>
2019-02-17 14:59:09 +0900

    保存タイミングの変更

--- alphazero_learn.cpp
+++ alphazero_learn.cpp
@@ -187,22 +187,17 @@ void alphaZero() {
             validation_log << step_num << "\t" << elapsedHours(start_time) << "\t"
                            << policy_loss_coeff * val_loss[0] + value_loss_coeff * val_loss[1] << "\t"
                            << val_loss[0] << "\t" << val_loss[1] << std::endl;
-
-            //保存
-#ifdef USE_LIBTORCH
-            torch::save(learning_model, MODEL_PREFIX + std::to_string(step_num) + ".model");
-#else
-            learning_model.save(MODEL_PREFIX + std::to_string(step_num) + ".model");
-#endif
         }
 
         if (step_num == 1 * max_step_num / 7 ||
             step_num == 3 * max_step_num / 7 ||
             step_num == 5 * max_step_num / 7) {
-            //学習率減衰
+            //モデルの保存と学習率減衰
 #ifdef USE_LIBTORCH
+            torch::save(learning_model, MODEL_PREFIX + std::to_string(step_num) + ".model");
             optimizer.options.learning_rate_ /= 10;
 #else
+            learning_model.save(MODEL_PREFIX + std::to_string(step_num) + ".model");
             optimizer.set_learning_rate_scaling(optimizer.get_learning_rate_scaling() / 10);
 #endif
         }
