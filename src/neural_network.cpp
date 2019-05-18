21323f311f530c496a5da3f8db600102d6bdda26
Shintaro Sakoda <rgbygscrsedppbwg@gmial.com>
2019-05-18 17:13:17 +0900

    FP16のやり方を変更

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -150,6 +150,19 @@ void NeuralNetworkImpl::setGPU(int16_t gpu_id) {
     device_ = (torch::cuda::is_available() ? torch::Device(torch::kCUDA, gpu_id) : torch::Device(torch::kCPU));
 #ifdef USE_HALF_FLOAT
     to(device_, torch::kHalf);
+    first_conv->to(device_, torch::kHalf);
+    first_bn->to(device_);
+    for (int32_t i = 0; i < BLOCK_NUM; i++) {
+        for (int32_t j = 0; j < 2; j++) {
+            conv[i][j]->to(device_, torch::kHalf);
+            bn[i][j]->to(device_);
+        }
+    }
+    policy_conv->to(device_, torch::kHalf);
+    value_conv->to(device_, torch::kHalf);
+    value_bn->to(device_);
+    value_fc1->to(device_, torch::kHalf);
+    value_fc2->to(device_, torch::kHalf);
 #else
     to(device_);
 #endif
