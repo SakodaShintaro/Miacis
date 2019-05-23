8d1a4ed8b3aba11b119042783a8a60747190a708
Shintaro Sakoda <rgbygscrsedppbwg@gmial.com>
2019-05-23 12:52:01 +0900

    一度しか使わない変数でも最初にまとめて取得してしまうように変更

--- src/alphazero_learn.cpp
+++ src/alphazero_learn.cpp
@@ -35,49 +35,52 @@ void alphaZero() {
         exit(1);
     }
 
-    //リプレイバッファ
-    ReplayBuffer replay_buffer(settings.get<int64_t>("first_wait"), settings.get<int64_t>("max_stack_size"), settings.get<float>("lambda"));
+    //値の取得
+    float learn_rate                 = settings.get<float>("learn_rate");
+    float momentum                   = settings.get<float>("momentum");
+    float policy_loss_coeff          = settings.get<float>("policy_loss_coeff");
+    float value_loss_coeff           = settings.get<float>("value_loss_coeff");
+    float lambda                     = settings.get<float>("lambda");
+    int64_t learn_rate_decay_step1   = settings.get<int64_t>("learn_rate_decay_step1");
+    int64_t learn_rate_decay_step2   = settings.get<int64_t>("learn_rate_decay_step2");
+    int64_t learn_rate_decay_step3   = settings.get<int64_t>("learn_rate_decay_step3");
+    int64_t draw_turn                = settings.get<int64_t>("draw_turn");
+    int64_t batch_size               = settings.get<int64_t>("batch_size");
+    int64_t thread_num               = settings.get<int64_t>("thread_num");
+    int64_t max_step_num             = settings.get<int64_t>("max_step_num");
+    int64_t update_interval          = settings.get<int64_t>("update_interval");
+    int64_t sleep_msec               = settings.get<int64_t>("sleep_msec");
+    int64_t maX_stack_size           = settings.get<int64_t>("max_stack_size");
+    int64_t first_wait               = settings.get<int64_t>("first_wait");
+    int64_t search_limit             = settings.get<int64_t>("search_limit");
+    int64_t search_batch_size        = settings.get<int64_t>("search_batch_size");
+    int64_t validation_interval      = settings.get<int64_t>("validation_interval");
+    int64_t validation_size          = settings.get<int64_t>("validation_size");
+    std::string validation_kifu_path = settings.get<std::string>("validation_kifu_path");
 
-    //usi_optionの設定
-    int64_t draw_turn         = settings.get<int64_t>("draw_turn");
-    int64_t thread_num        = settings.get<int64_t>("thread_num");
-    int64_t search_batch_size = settings.get<int64_t>("search_batch_size");
-    int64_t search_limit      = settings.get<int64_t>("search_limit");
+    //値同士の制約関係を確認
+    assert(validation_interval % update_interval == 0);
 
     //探索のシグナルをオフにしておく
     Searcher::stop_signal = false;
 
-    //学習ループ中で複数回参照するオプションは変数として確保する
-    //速度的な問題はほぼないと思うが,学習始まってからtypoで中断が入るのも嫌なので
-    uint64_t batch_size            = static_cast<uint64_t>(settings.get<int64_t>("batch_size"));
-    int64_t max_step_num           = settings.get<int64_t>("max_step_num");
-    int64_t learn_rate_decay_step1 = settings.get<int64_t>("learn_rate_decay_step1");
-    int64_t learn_rate_decay_step2 = settings.get<int64_t>("learn_rate_decay_step2");
-    int64_t learn_rate_decay_step3 = settings.get<int64_t>("learn_rate_decay_step3");
-    int64_t validation_interval    = settings.get<int64_t>("validation_interval");
-    int64_t update_interval        = settings.get<int64_t>("update_interval");
-    int64_t sleep_msec             = settings.get<int64_t>("sleep_msec");
-    float policy_loss_coeff        = settings.get<float>("policy_loss_coeff");
-    float value_loss_coeff         = settings.get<float>("value_loss_coeff");
-
-    assert(validation_interval % update_interval == 0);
+    //リプレイバッファ
+    ReplayBuffer replay_buffer(first_wait, maX_stack_size, lambda);
 
     //ログファイルの設定
     std::ofstream learn_log("alphazero_log.txt");
-    learn_log << "time\tstep\tloss\tpolicy_loss\tvalue_loss" << std::fixed << std::endl;
-    std::cout << "time\tstep\tloss\tpolicy_loss\tvalue_loss" << std::fixed << std::endl;
     std::ofstream validation_log("alphazero_validation_log.txt");
-    validation_log << "time\tstep\tsum_loss\tpolicy_loss\tvalue_loss" << std::fixed << std::endl;
+    dout(std::cout, learn_log) << "time\tstep\tsum_loss\tpolicy_loss\tvalue_loss" << std::fixed << std::endl;
+    validation_log             << "time\tstep\tsum_loss\tpolicy_loss\tvalue_loss" << std::fixed << std::endl;
 
     //データを取得
-    std::vector<std::pair<std::string, TeacherType>> validation_data;
-    validation_data = loadData(settings.get<std::string>("validation_kifu_path"));
-    assert(validation_data.size() >= settings.get<int64_t>("validation_size"));
+    std::vector<std::pair<std::string, TeacherType>> validation_data = loadData(validation_kifu_path);
+    assert(validation_data.size() >= validation_size);
 
     //データをシャッフルして必要量以外を削除
     std::mt19937_64 engine(0);
     std::shuffle(validation_data.begin(), validation_data.end(), engine);
-    validation_data.erase(validation_data.begin() + settings.get<int64_t>("validation_size"), validation_data.end());
+    validation_data.erase(validation_data.begin() + validation_size, validation_data.end());
     validation_data.shrink_to_fit();
 
     //モデル読み込み
@@ -90,8 +93,8 @@ void alphaZero() {
     torch::save(learning_model, MODEL_PREFIX + "_before_alphazero.model");
 
     //Optimizerの準備
-    torch::optim::SGDOptions sgd_option(settings.get<float>("learn_rate"));
-    sgd_option.momentum(settings.get<float>("momentum"));
+    torch::optim::SGDOptions sgd_option(learn_rate);
+    sgd_option.momentum(momentum);
     torch::optim::SGD optimizer(learning_model->parameters(), sgd_option);
 
     //時間計測開始
@@ -129,7 +132,7 @@ void alphaZero() {
         //1回目はmakeBatch内で十分棋譜が貯まるまで待ち時間が発生する.その生成速度を計算
         if (step_num == 1) {
             double h = elapsedHours(start_time);
-            std::cout << settings.get<int64_t>("first_wait") / (h * 3600) << " pos / sec" << std::endl;
+            std::cout << first_wait / (h * 3600) << " pos / sec" << std::endl;
         }
 
         //先頭のネットワークとはGPUを共有しているのでロックをかける
