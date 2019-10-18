5fe6c39bb619727d2098f0de22b27bceaae42e1e
Shintaro Sakoda <sakoda@keio.jp>
2019-10-18 16:56:02 +0900

    コピーする部分を追加

--- scripts/supervised_learn.sh
+++ scripts/supervised_learn.sh
@@ -2,3 +2,4 @@ cp ../../Miacis_scalar .
 cp ../../../../setting/supervised_learn_settings.txt .
 git show -s > git_commit_id.txt
 echo -e "initParams\nsupervisedLearn\nquit\n" | ./Miacis_scalar
+scp -r `pwd` sakoda:~/learn_result/supervised/
