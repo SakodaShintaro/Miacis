82e0f0fc7c7a17db91b64a60d1335d1b60b1e08f
Shintaro Sakoda <sakoda@keio.jp>
2019-10-22 16:23:24 +0900

    学習中はノイズを追加してみる

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -328,6 +328,10 @@ std::array<torch::Tensor, LOSS_TYPE_NUM> NeuralNetworkImpl::loss(const std::vect
         //i == 0のときはすぐ上の行で、それ以外のときは下のループ中で状態をエンコードしているのでそれを再利用
         torch::Tensor state_representation = next_state_representation;
 
+        if (is_training()) {
+            state_representation += torch::randn(state_representation.sizes()).to(device_);
+        }
+
         losses[i * STANDARD_LOSS_TYPE_NUM + POLICY_LOSS_INDEX] = policyLoss(state_representation, move_teachers[i]);
         losses[i * STANDARD_LOSS_TYPE_NUM + VALUE_LOSS_INDEX] = valueLoss(state_representation, value_teachers[i]);
         losses[i * STANDARD_LOSS_TYPE_NUM + RECONSTRUCT_LOSS_INDEX] = reconstructLoss(state_representation, board_teacher_vec[i], hand_teacher_vec[i]);
