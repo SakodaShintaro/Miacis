31daf47592bc1affb2248794e59e2bd0c787b5ce
Shintaro Sakoda <sakoda@keio.jp>
2019-10-25 16:32:13 +0900

    Revert "予測を差分計算"
    This reverts commit 37985bebd2e167d0d8ce557f4e88a899a64d1456.

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -181,8 +181,7 @@ torch::Tensor NeuralNetworkImpl::predictTransition(const torch::Tensor& state_re
     //手番が変わることを考慮して180度ひっくり返す
     x = torch::rot90(x, 2, { 2, 3 });
 
-    //差分計算する
-    return torch::rot90(state_representations, 2, { 2, 3 }) + x;
+    return x;
 }
 
 std::vector<FloatType> NeuralNetworkImpl::predictTransition(const std::vector<FloatType>& state_rep, Move move) {
