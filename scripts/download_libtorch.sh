23e5d60a7e85eae004346e579e963f1fc02139f3
sakoda <sakoda@keio.jp>
2020-04-17 10:23:18 +0900

    ダウンロードスクリプトを修正

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -3,6 +3,6 @@ cd `dirname $0`
 # どこに保存するかの基準位置
 root_dir=../..
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu100/libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
-unzip -q ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.2.0.zip -d ${root_dir}/
-rm ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu101/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip
+unzip -q ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip -d ${root_dir}/
+rm ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.4.0.zip
