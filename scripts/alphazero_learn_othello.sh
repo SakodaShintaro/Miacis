135d293563469a511409e84ffd98bf0fbbfd1bc4
sakoda <sakoda@keio.jp>
2020-04-25 11:54:03 +0900

    保存ディレクトリを修正

--- scripts/alphazero_learn_othello.sh
+++ scripts/alphazero_learn_othello.sh
@@ -2,4 +2,4 @@ git show -s > git_commit_id.txt
 git diff   >> git_commit_id.txt
 echo -e "initParams\nalphaZero\nquit\n" | ./Miacis_othello_*
 ~/Miacis/scripts/vsEdax.py
-scp -r `pwd` sakoda:~/learn_result/alphazero/
+scp -r `pwd` sakoda:~/learn_result/alphazero/othello/
