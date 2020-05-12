764af50d56a3b188189b7ef69dfcd2d74b5a9463
sakoda <sakoda@keio.jp>
2020-05-12 23:36:03 +0900

    デフォルトでexp_searchを使うように変更

--- scripts/alphazero_learn_othello.sh
+++ scripts/alphazero_learn_othello.sh
@@ -1,5 +1,5 @@
 git show -s > git_commit_id.txt
 git diff   >> git_commit_id.txt
 echo -e "initParams\nalphaZero\nquit\n" | ./Miacis_othello_*
-~/Miacis/scripts/vsEdax.py
+~/Miacis/scripts/vsEdax.py --exp_search
 scp -r `pwd` sakoda:~/learn_result/alphazero/othello/
