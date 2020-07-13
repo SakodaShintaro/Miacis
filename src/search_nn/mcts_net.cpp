4dde4901d8ef4ce2798fb75ea88c06e8ac1cdad9
sakoda <sakoda@keio.jp>
2020-07-13 15:28:50 +0900

    mctsのアルゴリズムを確認

--- src/search_nn/mcts_net.cpp
+++ src/search_nn/mcts_net.cpp
@@ -1,1 +1,39 @@
 #include "mcts_net.hpp"
+#include <stack>
+
+MCTSNet::MCTSNet(const SearchOptions& search_options) : search_options_(search_options),
+                                                        hash_table_(search_options.USI_Hash * 1024 * 1024 / 10000) {}
+
+Move MCTSNet::think(Position& root, int64_t time_limit) {
+    //思考を行う
+    //時間制限、あるいはノード数制限に基づいて何回やるかを決める
+    //基本的にはノード数制限のみを考えるイメージで最初は
+    for (int64_t _ = 0; _ < search_options_.search_limit; _++) {
+        //1回探索する
+
+        //到達したノードの履歴
+        std::stack<Index> indices;
+
+        //(1)選択
+        //特徴量作成
+        //Policy Networkに入力し、方策を推論
+        //行動をサンプリング
+
+        //(2)評価
+        //Embed Networkに入力し、埋め込みを取得
+        //置換表に登録
+
+        //(3)バックアップ
+        while (!indices.empty()) {
+            Index top = indices.top();
+            indices.pop();
+
+            //Backup Networkにより更新(差分更新)
+        }
+    }
+
+    //最終的な行動決定
+    //Readout Networkにより最終決定
+
+    //SoftMaxでの最大を行動選択
+}
--- src/search_nn/mcts_net.hpp
+++ src/search_nn/mcts_net.hpp
@@ -14,54 +14,18 @@ public:
     //探索を行って一番良い指し手を返す関数
     Move think(Position& root, int64_t time_limit);
 
-    const HashTableForMCTSNet& hashTable() { return hash_table_; }
-
-    //探索の終了判定。外部から探索を止めたい場合にはこれをtrueにする
-    bool stop_signal;
-
 private:
-    //時間制限含め探索を続けるかどうかを判定する関数
-    bool shouldStop();
-
-    //GPUに付随するスレッド。内部的に下のworkerThreadFuncをthread_num_per_gpu数だけ生成する
-    void gpuThreadFunc(const Position& root, int64_t gpu_id);
-
-    //各GPUの下で動くスレッド
-    void workerThreadFunc(Position root, int64_t gpu_id, int64_t thread_id);
-
-    //PVを取得する関数
-    std::vector<Move> getPV() const;
-
-    //探索結果をストリームに出力する関数
-    void outputInfo(std::ostream& ost, int64_t gather_num) const;
-
     //探索に関するオプション
     const SearchOptions& search_options_;
 
     //置換表は1個
     HashTableForMCTSNet hash_table_;
 
-    //GPUは複数
-    std::vector<NeuralNetwork> neural_networks_;
-    std::vector<std::mutex> gpu_mutexes_;
-
-    //時間
-    std::chrono::steady_clock::time_point start_;
-
-    //時間制限(msec),ノード数制限
-    int64_t time_limit_;
-    int64_t node_limit_;
-
-    //次に表示する経過時間
-    int64_t next_print_time_;
-
-    //ログファイルを出力する場合のストリーム
-    std::ofstream log_file_;
-
-#ifdef SHOGI
-    //定跡
-    Book book_;
-#endif
+    //使用するニューラルネットワーク
+    //policy network
+    //embed network
+    //backup network
+    //readout network
 };
 
 #endif //MIACIS_MCTS_NET_HPP
