80a5893c4725a33fb9566d19ea75376fffa09dc8
sakoda <sakoda@keio.jp>
2020-07-20 16:48:19 +0900

    提案手法のクラスを多少修正。これはDRC風味に変えてしまおうか

--- src/search_nn/proposed_lstm.cpp
+++ src/search_nn/proposed_lstm.cpp
@@ -20,6 +20,57 @@ ProposedModelImpl::ProposedModelImpl() {
     resetState();
 }
 
+ProposedModelImpl::ProposedModelImpl(const SearchOptions& search_options) : search_options_(search_options) {
+    torch::nn::LSTMOptions option(BOARD_WIDTH * BOARD_WIDTH * CHANNEL_NUM, HIDDEN_SIZE);
+    option.num_layers(NUM_LAYERS);
+    lstm_ = register_module("lstm_", torch::nn::LSTM(option));
+    policy_head_ = register_module("policy_head_", torch::nn::Linear(HIDDEN_SIZE, POLICY_DIM + 1));
+    value_head_ = register_module("value_head_", torch::nn::Linear(HIDDEN_SIZE, 1));
+    resetState();
+}
+
+Move ProposedModelImpl::think(Position& root, int64_t time_limit, bool save_info_to_learn) {
+    //思考を行う
+    //時間制限、あるいはノード数制限に基づいて何回やるかを決める
+    std::vector<torch::Tensor> outputs;
+
+    //最初のエンコード
+    torch::Tensor embed_vector;
+
+    for (int64_t m = 0; m < search_options_.search_limit; m++) {
+        //今までの探索から現時点での結論を推論
+
+
+        //LSTMでの探索を実行
+        auto[policy, value] = lstm_->forward(embed_vector, std::make_tuple(h_, c_));
+
+        //Policyからサンプリングして行動決定(undoを含む)
+
+        //盤面を遷移
+
+        //埋め込みベクトルを更新
+        embed_vector = embed_vector;
+    }
+
+    //合法手だけマスクをかける
+    std::vector<Move> moves = root.generateAllMoves();
+    std::vector<float> logits;
+    for (const Move& move : moves) {
+        logits.push_back(outputs.back()[0][move.toLabel()].item<float>());
+    }
+
+    if (root.turnNumber() <= search_options_.random_turn) {
+        //Softmaxの確率に従って選択
+        std::vector<float> masked_policy = softmax(logits, 1.0f);
+        int32_t move_id = randomChoose(masked_policy);
+        return moves[move_id];
+    } else {
+        //最大のlogitを持つ行動を選択
+        int32_t move_id = std::max_element(logits.begin(), logits.end()) - logits.begin();
+        return moves[move_id];
+    }
+}
+
 std::tuple<torch::Tensor, torch::Tensor> ProposedModelImpl::forward(const torch::Tensor& x) {
     //lstmは入力(input, (h_0, c_0))
     //inputのshapeは(seq_len, batch, input_size)
--- src/search_nn/proposed_lstm.hpp
+++ src/search_nn/proposed_lstm.hpp
@@ -1,6 +1,8 @@
 #ifndef MIACIS_PROPOSED_LSTM_HPP
 #define MIACIS_PROPOSED_LSTM_HPP
 
+#include"../search_options.hpp"
+#include"../include_switch.hpp"
 #include<torch/torch.h>
 
 //提案手法で用いるNN
@@ -10,10 +12,18 @@
 class ProposedModelImpl : public torch::nn::Module {
 public:
     ProposedModelImpl();
+    explicit ProposedModelImpl(const SearchOptions& search_options);
+
+    //探索を行って一番良い指し手を返す関数
+    Move think(Position& root, int64_t time_limit, bool save_info_to_learn = false);
+
     std::tuple<torch::Tensor, torch::Tensor> forward(const torch::Tensor& x);
     torch::Tensor loss(const torch::Tensor& x, const torch::Tensor& t);
     void resetState();
 private:
+    //探索に関するオプション
+    SearchOptions search_options_;
+
     torch::nn::LSTM lstm_{ nullptr };
     torch::nn::Linear policy_head_{ nullptr };
     torch::nn::Linear value_head_{ nullptr };
