87f1a8efb33d1ff8e52e2ce0211508b1f3e3cfba
sakoda <sakoda@keio.jp>
2020-07-21 10:28:26 +0900

    提案モデルをある程度整えた

--- src/search_nn/proposed_lstm.cpp
+++ src/search_nn/proposed_lstm.cpp
@@ -1,4 +1,6 @@
 #include "proposed_lstm.hpp"
+
+#include <utility>
 #include "../include_switch.hpp"
 
 //ネットワークの設定
@@ -7,25 +9,30 @@ static constexpr int32_t BLOCK_NUM = 10;
 #elif OTHELLO
 static constexpr int32_t BLOCK_NUM = 5;
 #endif
+static constexpr int32_t KERNEL_SIZE = 3;
+static constexpr int32_t REDUCTION = 8;
 static constexpr int32_t CHANNEL_NUM = 64;
+static constexpr int64_t HIDDEN_CHANNEL_NUM = 32;
+static constexpr int64_t HIDDEN_DIM = BOARD_WIDTH * BOARD_WIDTH * HIDDEN_CHANNEL_NUM;
 static constexpr int32_t HIDDEN_SIZE = 512;
 static constexpr int32_t NUM_LAYERS = 2;
 
-ProposedModelImpl::ProposedModelImpl() {
-    torch::nn::LSTMOptions option(BOARD_WIDTH * BOARD_WIDTH * CHANNEL_NUM, HIDDEN_SIZE);
-    option.num_layers(NUM_LAYERS);
-    lstm_ = register_module("lstm_", torch::nn::LSTM(option));
-    policy_head_ = register_module("policy_head_", torch::nn::Linear(HIDDEN_SIZE, POLICY_DIM + 1));
-    value_head_ = register_module("value_head_", torch::nn::Linear(HIDDEN_SIZE, 1));
-    resetState();
-}
+ProposedModelImpl::ProposedModelImpl(SearchOptions search_options) : search_options_(std::move(search_options)),
+                                                                     blocks_(BLOCK_NUM, nullptr), device_(torch::kCUDA), fp16_(false) {
+    first_conv_ = register_module("first_conv_", Conv2DwithBatchNorm(INPUT_CHANNEL_NUM, CHANNEL_NUM, KERNEL_SIZE));
+    for (int32_t i = 0; i < BLOCK_NUM; i++) {
+        blocks_[i] = register_module("blocks_" + std::to_string(i), ResidualBlock(CHANNEL_NUM, KERNEL_SIZE, REDUCTION));
+    }
+    last_conv_ = register_module("last_conv_", Conv2DwithBatchNorm(CHANNEL_NUM, HIDDEN_CHANNEL_NUM, KERNEL_SIZE));
 
-ProposedModelImpl::ProposedModelImpl(const SearchOptions& search_options) : search_options_(search_options) {
-    torch::nn::LSTMOptions option(BOARD_WIDTH * BOARD_WIDTH * CHANNEL_NUM, HIDDEN_SIZE);
+    torch::nn::LSTMOptions option(HIDDEN_DIM, HIDDEN_SIZE);
     option.num_layers(NUM_LAYERS);
-    lstm_ = register_module("lstm_", torch::nn::LSTM(option));
-    policy_head_ = register_module("policy_head_", torch::nn::Linear(HIDDEN_SIZE, POLICY_DIM + 1));
-    value_head_ = register_module("value_head_", torch::nn::Linear(HIDDEN_SIZE, 1));
+    simulation_lstm_ = register_module("simulation_lstm_", torch::nn::LSTM(option));
+    simulation_policy_head_ = register_module("simulation_policy_head_", torch::nn::Linear(HIDDEN_SIZE, POLICY_DIM + 1));
+    simulation_value_head_ = register_module("simulation_value_head_", torch::nn::Linear(HIDDEN_SIZE, 1));
+    readout_lstm_ = register_module("readout_lstm_", torch::nn::LSTM(option));
+    readout_policy_head_ = register_module("readout_policy_head_", torch::nn::Linear(HIDDEN_SIZE, POLICY_DIM + 1));
+    readout_value_head_ = register_module("readout_value_head_", torch::nn::Linear(HIDDEN_SIZE, 1));
     resetState();
 }
 
@@ -35,14 +42,14 @@ Move ProposedModelImpl::think(Position& root, int64_t time_limit, bool save_info
     std::vector<torch::Tensor> outputs;
 
     //最初のエンコード
-    torch::Tensor embed_vector;
+    torch::Tensor embed_vector = embed(root.makeFeature());
 
     for (int64_t m = 0; m < search_options_.search_limit; m++) {
         //今までの探索から現時点での結論を推論
-
+        auto[readout_policy, readout_value] = readoutPolicy(embed_vector);
 
         //LSTMでの探索を実行
-        auto[policy, value] = lstm_->forward(embed_vector, std::make_tuple(h_, c_));
+        auto[simulation_policy, simulation_value] = simulationPolicy(embed_vector);
 
         //Policyからサンプリングして行動決定(undoを含む)
 
@@ -71,7 +78,19 @@ Move ProposedModelImpl::think(Position& root, int64_t time_limit, bool save_info
     }
 }
 
-std::tuple<torch::Tensor, torch::Tensor> ProposedModelImpl::forward(const torch::Tensor& x) {
+torch::Tensor ProposedModelImpl::embed(const std::vector<float>& inputs) {
+    torch::Tensor x = (fp16_ ? torch::tensor(inputs).to(device_, torch::kHalf) : torch::tensor(inputs).to(device_));
+    x = x.view({ -1, INPUT_CHANNEL_NUM, BOARD_WIDTH, BOARD_WIDTH });
+    torch::Tensor y = first_conv_->forward(x);
+    for (ResidualBlock& block : blocks_) {
+        y = block->forward(y);
+    }
+    y = last_conv_->forward(y);
+    y = torch::flatten(y, 1);
+    return y;
+}
+
+std::tuple<torch::Tensor, torch::Tensor> ProposedModelImpl::simulationPolicy(const torch::Tensor& x) {
     //lstmは入力(input, (h_0, c_0))
     //inputのshapeは(seq_len, batch, input_size)
     //h_0, c_0は任意の引数で、状態を初期化できる
@@ -79,24 +98,41 @@ std::tuple<torch::Tensor, torch::Tensor> ProposedModelImpl::forward(const torch:
 
     //出力はoutput, (h_n, c_n)
     //outputのshapeは(seq_len, batch, num_directions * hidden_size)
-    auto[output, h_and_c] = lstm_->forward(x, std::make_tuple(h_, c_));
-    std::tie(h_, c_) = h_and_c;
+    auto[output, h_and_c] = simulation_lstm_->forward(x, std::make_tuple(simulation_h_, simulation_c_));
+    std::tie(simulation_h_, simulation_c_) = h_and_c;
 
-    torch::Tensor policy = policy_head_->forward(output);
-    torch::Tensor value = value_head_->forward(output);
+    torch::Tensor policy = simulation_policy_head_->forward(output);
+    torch::Tensor value = simulation_value_head_->forward(output);
+
+    return std::make_tuple(policy, value);
+}
+
+std::tuple<torch::Tensor, torch::Tensor> ProposedModelImpl::readoutPolicy(const torch::Tensor& x) {
+    //lstmは入力(input, (h_0, c_0))
+    //inputのshapeは(seq_len, batch, input_size)
+    //h_0, c_0は任意の引数で、状態を初期化できる
+    //h_0, c_0のshapeは(num_layers_ * num_directions, batch, hidden_size_)
+
+    //出力はoutput, (h_n, c_n)
+    //outputのshapeは(seq_len, batch, num_directions * hidden_size)
+    auto[output, h_and_c] = readout_lstm_->forward(x, std::make_tuple(readout_h_, readout_c_));
+    std::tie(readout_h_, readout_c_) = h_and_c;
+
+    torch::Tensor policy = readout_policy_head_->forward(output);
+    torch::Tensor value = readout_value_head_->forward(output);
 
     return std::make_tuple(policy, value);
 }
 
 torch::Tensor ProposedModelImpl::loss(const torch::Tensor& x, const torch::Tensor& t) {
-    auto[policy, value] = forward(x);
-
-    //policyについての損失
-    std::vector<float> losses;
-    for (int64_t i = 0; i < x.size(0); i++) {
-        //最終結果と比較して損失を貯めていく
-
-    }
+//    auto[policy, value] = simulationPolicy(x);
+//
+//    //policyについての損失
+//    std::vector<float> losses;
+//    for (int64_t i = 0; i < x.size(0); i++) {
+//        //最終結果と比較して損失を貯めていく
+//
+//    }
 
     //差分を計算する
 
@@ -109,6 +145,8 @@ torch::Tensor ProposedModelImpl::loss(const torch::Tensor& x, const torch::Tenso
 
 void ProposedModelImpl::resetState() {
     //(num_layers * num_directions, batch, hidden_size)
-    h_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
-    c_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
+    simulation_h_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
+    simulation_c_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
+    readout_h_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
+    readout_c_ = torch::zeros({ NUM_LAYERS, 1, HIDDEN_SIZE });
 }
--- src/search_nn/proposed_lstm.hpp
+++ src/search_nn/proposed_lstm.hpp
@@ -11,24 +11,56 @@
 
 class ProposedModelImpl : public torch::nn::Module {
 public:
-    ProposedModelImpl();
-    explicit ProposedModelImpl(const SearchOptions& search_options);
+    ProposedModelImpl() : ProposedModelImpl(SearchOptions()) {}
+    explicit ProposedModelImpl(SearchOptions search_options);
 
     //探索を行って一番良い指し手を返す関数
     Move think(Position& root, int64_t time_limit, bool save_info_to_learn = false);
 
-    std::tuple<torch::Tensor, torch::Tensor> forward(const torch::Tensor& x);
     torch::Tensor loss(const torch::Tensor& x, const torch::Tensor& t);
     void resetState();
 private:
+    torch::Tensor embed(const std::vector<float>& inputs);
+
+    std::tuple<torch::Tensor, torch::Tensor> simulationPolicy(const torch::Tensor& x);
+    std::tuple<torch::Tensor, torch::Tensor> readoutPolicy(const torch::Tensor& x);
+
     //探索に関するオプション
     SearchOptions search_options_;
 
-    torch::nn::LSTM lstm_{ nullptr };
-    torch::nn::Linear policy_head_{ nullptr };
-    torch::nn::Linear value_head_{ nullptr };
-    torch::Tensor h_;
-    torch::Tensor c_;
+    //---------------
+    //    Encoder
+    //---------------
+    //最初にチャンネル数を変えるConv
+    Conv2DwithBatchNorm first_conv_{ nullptr };
+
+    //同じチャンネル数で残差ブロックを通す
+    std::vector<ResidualBlock> blocks_;
+
+    //最後にチャンネル数を絞るConv
+    Conv2DwithBatchNorm last_conv_{ nullptr };
+
+    //-------------------------
+    //    Simulation Policy
+    //-------------------------
+    torch::nn::LSTM simulation_lstm_{ nullptr };
+    torch::nn::Linear simulation_policy_head_{ nullptr };
+    torch::nn::Linear simulation_value_head_{ nullptr };
+    torch::Tensor simulation_h_;
+    torch::Tensor simulation_c_;
+
+    //----------------------
+    //    Readout Policy
+    //----------------------
+    torch::nn::LSTM readout_lstm_{ nullptr };
+    torch::nn::Linear readout_policy_head_{ nullptr };
+    torch::nn::Linear readout_value_head_{ nullptr };
+    torch::Tensor readout_h_;
+    torch::Tensor readout_c_;
+
+    //デバイスとfp16化
+    torch::Device device_;
+    bool fp16_;
 };
 TORCH_MODULE(ProposedModel);
 
