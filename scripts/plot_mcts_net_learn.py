45c27e4a074d9fea900ac54f30afb97aa0ddc81c
sakoda <sakoda@keio.jp>
2020-07-23 10:13:25 +0900

    プロットスクリプトを改修

--- scripts/plot_mcts_net_learn.py
+++ scripts/plot_mcts_net_learn.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import japanize_matplotlib
-import argparse
 
-f = open("./mcts_net_train_log.txt")
-
-step = list()
 M = 10
-INTERVAL = 250
-losses = [list() for _ in range(M)]
+PLOT_NUM = 20
 
-for line in f:
-    elements = line.strip().split()
-    print(elements)
-    if elements[0] == "time":
-        continue
-    step.append(int(elements[2]))
-    for i in range(M):
-        losses[i].append(float(elements[4 + i]))
 
 def transform(arr):
+    arr = arr[0:len(arr) // PLOT_NUM * PLOT_NUM]
     arr = np.array(arr)
-    arr = arr.reshape([-1, INTERVAL])
+    arr = arr.reshape([PLOT_NUM, -1])
     return arr.mean(1)
 
-step = transform(step)
 
-for i in range(M):
-    losses[i] = transform(losses[i])
+for loss_name in ["train", "valid"]:
+    f = open(f"./mcts_net_{loss_name}_log.txt")
 
-for i in range(M):
-    plt.plot(step, losses[i])
-    plt.text(step[-1], losses[i][-1], f"{i + 1}回探索後", color=plt.get_cmap("tab10")(i))
-plt.xlim((plt.xlim()[0], plt.xlim()[1] * 1.15))
-plt.xlabel("学習ステップ数")
-plt.ylabel("Policy損失")
-plt.savefig("mcts_net_learn.png", bbox_inches="tight", pad_inches=0.05)
+    step = list()
+    losses = [list() for _ in range(M)]
+
+    # データ読み込み
+    for line in f:
+        elements = line.strip().split()
+        if elements[0] == "time":
+            continue
+        step.append(int(elements[2]))
+        for i in range(M):
+            losses[i].append(float(elements[4 + i]))
+
+    if loss_name == "train":
+        # 数が多いので適当な間隔で平均を取る
+        step = transform(step)
+
+        for i in range(M):
+            losses[i] = transform(losses[i])
+
+    for i in range(M):
+        plt.plot(step, losses[i])
+        plt.text(step[-1], losses[i][-1], f"{i + 1}回探索後", color=plt.get_cmap("tab10")(i))
+    plt.xlim((plt.xlim()[0], plt.xlim()[1] * 1.15))
+    plt.xlabel("学習ステップ数")
+    plt.ylabel("Policy損失")
+    plt.savefig(f"mcts_net_{loss_name}.png", bbox_inches="tight", pad_inches=0.05)
+    plt.clf()
