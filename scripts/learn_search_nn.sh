0976ca7f94a5364e8ab02868287bd13db8116c83
sakoda <sakoda@keio.jp>
2020-07-31 10:40:06 +0900

    学習スクリプトを準備

---
+++ scripts/learn_search_nn.sh
@@ -0,0 +1,4 @@
+git show -s > git_commit_id.txt
+git diff   >> git_commit_id.txt
+echo -e "learnMCTSNet\nlearnProposedModel\nlearnStackedLSTM\nlearnSimpleMLP\nquit" | ./Miacis_*
+scp -r `pwd` sakoda:~/learn_result/search_nn/
