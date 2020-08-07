3694ade1d1292b37009dfe3091dcb4a552578fb8
sakoda <sakoda@keio.jp>
2020-08-07 14:17:26 +0900

    ダウンロードをtrainとvalidに分けるようにした

--- scripts/download_Othello_kifu.sh
+++ scripts/download_Othello_kifu.sh
@@ -6,10 +6,18 @@ root_dir=../..
 #
 # 棋譜のダウンロード
 #
-path=${root_dir}/othello_kifu
-mkdir ${path}
-for i in `seq -f '%02g' 128`
+train_path=${root_dir}/othello_train_kifu
+mkdir ${train_path}
+for i in `seq -f '%02g' 1 100`
 do
-  wget -P ${path} "https://www.skatgame.net/mburo/ggs/game-archive/Othello/Othello.${i}e4.ggf.bz2"
-  bzip2 -d ${path}/Othello.${i}e4.ggf.bz2
+  wget -P ${train_path} "https://www.skatgame.net/mburo/ggs/game-archive/Othello/Othello.${i}e4.ggf.bz2"
+  bzip2 -d ${train_path}/Othello.${i}e4.ggf.bz2
+done
+
+valid_path=${root_dir}/othello_valid_kifu
+mkdir ${valid_path}
+for i in `seq -f '%02g' 101 128`
+do
+  wget -P ${valid_path} "https://www.skatgame.net/mburo/ggs/game-archive/Othello/Othello.${i}e4.ggf.bz2"
+  bzip2 -d ${valid_path}/Othello.${i}e4.ggf.bz2
 done
