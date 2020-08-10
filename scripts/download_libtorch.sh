24fe3342587f394ab170e82381966c4a15a6e5fa
sakoda <sakoda@keio.jp>
2020-08-10 14:22:55 +0900

    libtorch1.6.0へ対応するようにダウンロードスクリプトおよびCMakeLists.txtを修正。まだ検証は済んでいない

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -3,6 +3,8 @@ cd `dirname $0`
 # どこに保存するかの基準位置
 root_dir=../..
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.5.0.zip
-unzip -q ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.5.0.zip -d ${root_dir}/
-rm ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.5.0.zip
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip
+unzip -q ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip -d ${root_dir}/libtorch-tmp
+mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-1.6.0
+rm ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip
+rmdir ${root_dir}/libtorch-tmp
