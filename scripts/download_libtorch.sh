d87cd46f1b7817e8842b18afc704d73bda5cf19d
sakoda <sakoda@keio.jp>
2020-08-13 10:09:06 +0900

    ダウンロードスクリプトの変数を修正

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -3,8 +3,11 @@ cd `dirname $0`
 # どこに保存するかの基準位置
 root_dir=../..
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip
-unzip -q ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip -d ${root_dir}/libtorch-tmp
-mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-1.6.0
-rm ${root_dir}/libtorch-cxx11-abi-shared-with-deps-1.6.0.zip
+version=1.6.0
+file_name=libtorch-cxx11-abi-shared-with-deps-${version}.zip
+
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/${file_name}
+unzip -q ${root_dir}/${file_name} -d ${root_dir}/libtorch-tmp
+mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}
+rm ${root_dir}/${file_name}
 rmdir ${root_dir}/libtorch-tmp
