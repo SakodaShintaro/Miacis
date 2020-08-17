f42c6999822af38f8562fa567d7ca6327a053500
sakoda <sakoda@keio.jp>
2020-08-17 22:36:20 +0900

    凍結を試すのでsimpleMLPは廃止

--- scripts/learn_search_nn.sh
+++ scripts/learn_search_nn.sh
@@ -1,4 +1,4 @@
 git show -s > git_commit_id.txt
 git diff   >> git_commit_id.txt
-echo -e "learnMCTSNet\nlearnProposedModel\nlearnStackedLSTM\nlearnSimpleMLP\nquit" | ./Miacis_*
+echo -e "learnMCTSNet\nlearnProposedModel\nlearnStackedLSTM\nquit" | ./Miacis_*
 scp -r `pwd` sakoda:~/learn_result/search_nn/
