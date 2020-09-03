207d0e92f3b31fe14efd5a5f4100a3e160cb36bb
sakoda <sakoda@keio.jp>
2020-09-03 10:33:43 +0900

    trainについてもラベルを正しく修正

--- scripts/plot_search_nn_learn.py
+++ scripts/plot_search_nn_learn.py
@@ -40,7 +40,9 @@ for prefix in args.method:
             if loss_name == "train":
                 loss = transform(loss)
             color = [0.0, 0.5, i / loss_num]
-            plt.plot(step, loss, color=color, label=f"{i}回探索後")
+            plt.plot(step, loss, color=color, label=f"{i}回探索後" if loss_name == "valid" else (
+                f"{loss_num - 1}回探索後の損失" if i == 0 else f"{i}回目探索の寄与と確率の積"),
+                     linestyle=("dashed" if i == 0 else "solid"))
             # plt.text(step[-1], loss[-1], f"{i + 1}回探索後", color=color)
         plt.legend()
         # plt.xlim((plt.xlim()[0], plt.xlim()[1] * 1.15))
