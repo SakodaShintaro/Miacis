6d357f301d17cb2de4d0ab59cd2d4311de8efefb
sakoda <sakoda@keio.jp>
2020-09-25 09:38:02 +0900

    学習用シェルスクリプトを追加

---
+++ scripts/learn_stacked_lstm.sh
@@ -0,0 +1,4 @@
+git show -s >git_commit_id.txt
+git diff >>git_commit_id.txt
+echo -e "learnStackedLSTM\nquit" | ./Miacis_*
+scp -r $(pwd) sakoda:~/learn_result/search_nn/
---
+++ scripts/pretrain.sh
@@ -0,0 +1,4 @@
+git show -s >git_commit_id.txt
+git diff >>git_commit_id.txt
+echo -e "pretrain\nquit" | ./Miacis_*
+scp -r $(pwd) sakoda:~/learn_result/search_nn/
