8695b9eb5840542e7895e0ab7b1c93e3cee12b76
sakoda <sakoda@keio.jp>
2020-09-30 16:10:15 +0900

    entropyのインデックスが間違っていたのを修正

--- src/search_nn/learn_search_nn.hpp
+++ src/search_nn/learn_search_nn.hpp
@@ -167,7 +167,7 @@ template<class T> void learnSearchNN(const std::string& model_name) {
                 dout(std::cout, valid_log) << "\t" << valid_loss_sum[loss.size() - 2];
 
                 //entropy
-                dout(std::cout, valid_log) << "\t" << valid_loss_sum[loss.size() - 2] << std::endl;
+                dout(std::cout, valid_log) << "\t" << valid_loss_sum[loss.size() - 1] << std::endl;
             }
             if (global_step % save_interval == 0) {
                 //学習中のパラメータを書き出す
