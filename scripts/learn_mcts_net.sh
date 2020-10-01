a6bb277a1272e734d63c8e8c22584f36b323508f
sakoda <sakoda@keio.jp>
2020-10-01 10:16:18 +0900

    学習後に検証対局を自動で入れるように変更

--- scripts/learn_mcts_net.sh
+++ scripts/learn_mcts_net.sh
@@ -3,6 +3,6 @@ git diff >>git_commit_id.txt
 echo -e "learnMCTSNet\nquit" | ./Miacis_*
 ~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=0 --mcts_net
 ~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=10 --mcts_net
-~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=0  --mcts_net --use_readout_only
-~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=10  --mcts_net --use_readout_only
+~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=0 --mcts_net --use_readout_only
+~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=10 --mcts_net --use_readout_only
 scp -r $(pwd) sakoda:~/learn_result/search_nn/
