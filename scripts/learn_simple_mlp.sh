20106bcbc63992905d7906461d8c6c73570bf773
sakoda <sakoda@keio.jp>
2020-10-28 23:44:23 +0900

    simple_mlpのときはsearch_limitは0だけ

--- scripts/learn_simple_mlp.sh
+++ scripts/learn_simple_mlp.sh
@@ -2,5 +2,4 @@ git show -s >git_commit_id.txt
 git diff >>git_commit_id.txt
 echo -e "learnSimpleMLP\nquit" | ./Miacis_*
 ~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=0 --simple_mlp
-~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --search_limit=10 --simple_mlp
 scp -r $(pwd) sakoda:~/learn_result/search_nn/
