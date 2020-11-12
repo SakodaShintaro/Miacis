a93bd81e280cfbd565887395f6cf23e63694e616
sakoda <sakoda@keio.jp>
2020-11-12 10:49:58 +0900

    sim_policy_logitを丸ごとcpuに持ってきた方が高速であることが判明

--- src/search_nn/proposed_model_transformer/proposed_model_transformer.cpp
+++ src/search_nn/proposed_model_transformer/proposed_model_transformer.cpp
@@ -51,7 +51,7 @@ std::vector<std::tuple<torch::Tensor, torch::Tensor>> ProposedModelTransformerIm
         }
 
         //探索行動を決定
-        torch::Tensor sim_policy_logit = base_policy_head_->forward(x);
+        torch::Tensor sim_policy_logit = base_policy_head_->forward(x).cpu();
 
         //行動をサンプリングして盤面を動かす
         for (int64_t i = 0; i < batch_size; i++) {
