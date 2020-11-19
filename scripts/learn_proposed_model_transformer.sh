25fca92d1fba42cde0ca305f7b26874a2353e305
sakoda <sakoda@keio.jp>
2020-11-19 15:51:16 +0900

    デフォルトのsearch_limitを12に変更

--- scripts/learn_proposed_model_transformer.sh
+++ scripts/learn_proposed_model_transformer.sh
@@ -2,5 +2,5 @@ git show -s >git_commit_id.txt
 git diff >>git_commit_id.txt
 echo -e "learnProposedModelTransformer\nquit" | ./Miacis_*
 ~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --method_name="proposed_model_transformer" --search_limit=0
-~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --method_name="proposed_model_transformer" --search_limit=10
+~/Miacis/scripts/vsEdax.py --game_num=1000 --level=1 --method_name="proposed_model_transformer" --search_limit=12
 scp -r $(pwd) sakoda:~/learn_result/search_nn/
