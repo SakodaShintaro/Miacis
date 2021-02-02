3891525d92a6c1f0e9931039f5eb4bf69130693e
sakoda <sakoda@keio.jp>
2021-02-02 11:40:16 +0000

    fix typo

--- scripts/reinforcement_learn.sh
+++ scripts/reinforcement_learn.sh
@@ -1,6 +1,6 @@
 git show -s >git_commit_id.txt
 git diff >>git_commit_id.txt
-~/Miacis/script/generate_torch_script_model.py
+~/Miacis/scripts/generate_torch_script_model.py
 echo -e "reinforcementLearn\nquit\n" | ./Miacis_*
 zip -rq learn_kifu.zip learn_kifu
 rm -rf learn_kifu
--- scripts/supervised_learn.sh
+++ scripts/supervised_learn.sh
@@ -1,5 +1,5 @@
 git show -s > git_commit_id.txt
 git diff   >> git_commit_id.txt
-~/Miacis/script/generate_torch_script_model.py
+~/Miacis/scripts/generate_torch_script_model.py
 echo -e "supervisedLearn\nquit\n" | ./Miacis_*
 scp -r `pwd` sakoda:~/learn_result/supervised/
