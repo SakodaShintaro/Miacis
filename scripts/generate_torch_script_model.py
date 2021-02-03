12c4210756a8531db5705f95503842ef7253d747
sakoda <sakoda@keio.jp>
2021-02-03 04:46:24 +0000

    モデル生成スクリプトを改良

--- scripts/generate_torch_script_model.py
+++ scripts/generate_torch_script_model.py
@@ -14,17 +14,16 @@ args = parser.parse_args()
 
 REDUCTION = 8
 KERNEL_SIZE = 3
+VALUE_HIDDEN_NUM = 256
 
 if args.game == "shogi":
     INPUT_CHANNEL_NUM = 42
     BOARD_SIZE = 9
     POLICY_CHANNEL_NUM = 27
-    VALUE_HIDDEN_NUM = 256
 elif args.game == "othello":
     INPUT_CHANNEL_NUM = 2
     BOARD_SIZE = 8
     POLICY_CHANNEL_NUM = 2
-    VALUE_HIDDEN_NUM = 256
 
 
 class Conv2DwithBatchNorm(nn.Module):
@@ -37,6 +36,7 @@ class Conv2DwithBatchNorm(nn.Module):
         t = self.conv_.forward(x)
         t = self.norm_.forward(t)
         return t
+
 
 class ResidualBlock(nn.Module):
     def __init__(self, channel_num, kernel_size, reduction):
@@ -65,64 +65,76 @@ class ResidualBlock(nn.Module):
         return t
 
 
-class ScalarNetwork(nn.Module):
+class Encoder(nn.Module):
     def __init__(self, channel_num):
-        super(ScalarNetwork, self).__init__()
-
+        super(Encoder, self).__init__()
         self.first_conv_and_norm_ = Conv2DwithBatchNorm(INPUT_CHANNEL_NUM, channel_num, 3)
         self.blocks = nn.Sequential()
         for i in range(args.block_num):
             self.blocks.add_module(f"block{i}", ResidualBlock(channel_num, KERNEL_SIZE, REDUCTION))
-        self.policy_conv_ = nn.Conv2d(channel_num, POLICY_CHANNEL_NUM, 1, bias=True, padding=0)
-        self.value_conv_and_norm_ = Conv2DwithBatchNorm(channel_num, channel_num, 1)
-        self.value_linear0_ = nn.Linear(BOARD_SIZE * BOARD_SIZE * channel_num, VALUE_HIDDEN_NUM)
-        self.value_linear1_ = nn.Linear(VALUE_HIDDEN_NUM, 1)
 
     def forward(self, x):
         x = self.first_conv_and_norm_.forward(x)
         x = F.relu(x)
-
         x = self.blocks.forward(x)
-        policy = self.policy_conv_.forward(x)
+        return x
 
+
+class PolicyHead(nn.Module):
+    def __init__(self, channel_num):
+        super(PolicyHead, self).__init__()
+        self.policy_conv_ = nn.Conv2d(channel_num, POLICY_CHANNEL_NUM, 1, bias=True, padding=0)
+
+    def forward(self, x):
+        policy = self.policy_conv_.forward(x)
+        return policy
+
+
+class ValueHead(nn.Module):
+    def __init__(self, channel_num, unit_num):
+        super(ValueHead, self).__init__()
+        self.value_conv_and_norm_ = Conv2DwithBatchNorm(channel_num, channel_num, 1)
+        self.value_linear0_ = nn.Linear(BOARD_SIZE * BOARD_SIZE * channel_num, VALUE_HIDDEN_NUM)
+        self.value_linear1_ = nn.Linear(VALUE_HIDDEN_NUM, unit_num)
+
+    def forward(self, x):
         value = self.value_conv_and_norm_.forward(x)
         value = F.relu(value)
         value = value.view([-1, args.channel_num * BOARD_SIZE * BOARD_SIZE])
         value = self.value_linear0_.forward(value)
         value = F.relu(value)
         value = self.value_linear1_.forward(value)
-        value = torch.tanh(value)
+        return value
 
+
+class ScalarNetwork(nn.Module):
+    def __init__(self, channel_num):
+        super(ScalarNetwork, self).__init__()
+        self.encoder_ = Encoder(channel_num)
+        self.policy_head_ = PolicyHead(channel_num)
+        self.value_head_ = ValueHead(channel_num, 1)
+
+    def forward(self, x):
+        x = self.encoder_.forward(x)
+        policy = self.policy_head_.forward(x)
+        value = self.value_head_.forward(x)
+        value = torch.tanh(value)
         return policy, value
+
 
 class CategoricalNetwork(nn.Module):
     def __init__(self, channel_num):
         super(CategoricalNetwork, self).__init__()
-
-        self.first_conv_and_norm_ = Conv2DwithBatchNorm(INPUT_CHANNEL_NUM, channel_num, 3)
-        self.blocks = nn.Sequential()
-        for i in range(args.block_num):
-            self.blocks.add_module(f"block{i}", ResidualBlock(channel_num, KERNEL_SIZE, REDUCTION))
-        self.policy_conv_ = nn.Conv2d(channel_num, POLICY_CHANNEL_NUM, 1, bias=True, padding=0)
-        self.value_conv_and_norm_ = Conv2DwithBatchNorm(channel_num, channel_num, 1)
-        self.value_linear0_ = nn.Linear(BOARD_SIZE * BOARD_SIZE * channel_num, VALUE_HIDDEN_NUM)
-        self.value_linear1_ = nn.Linear(VALUE_HIDDEN_NUM, 51)
+        self.encoder_ = Encoder(channel_num)
+        self.policy_head_ = PolicyHead(channel_num)
+        self.value_head_ = ValueHead(channel_num, 51)
 
     def forward(self, x):
-        x = self.first_conv_and_norm_.forward(x)
-        x = F.relu(x)
-
-        x = self.blocks.forward(x)
-        policy = self.policy_conv_.forward(x)
-
-        value = self.value_conv_and_norm_.forward(x)
-        value = F.relu(value)
-        value = value.view([-1, args.channel_num * BOARD_SIZE * BOARD_SIZE])
-        value = self.value_linear0_.forward(value)
-        value = F.relu(value)
-        value = self.value_linear1_.forward(value)
-
+        x = self.encoder_.forward(x)
+        policy = self.policy_head_.forward(x)
+        value = self.value_head_.forward(x)
         return policy, value
+
 
 model = None
 if args.value_type == "sca":
@@ -132,4 +144,6 @@ elif args.value_type == "cat":
 input_data = torch.randn([8, INPUT_CHANNEL_NUM, BOARD_SIZE, BOARD_SIZE])
 script_model = torch.jit.trace(model, input_data)
 # script_model = torch.jit.script(model)
-script_model.save(f"./{args.game}_{args.value_type}_bl{args.block_num}_ch{args.channel_num}.model")
+model_path = f"./{args.game}_{args.value_type}_bl{args.block_num}_ch{args.channel_num}.model"
+script_model.save(model_path)
+print(f"{model_path}にパラメータを保存")
