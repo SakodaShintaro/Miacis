0958018813b65949e3dc1805ac5515637f9025a0
sakoda <sakoda@keio.jp>
2021-04-14 10:03:03 +0900

    不要な転送を削除

--- scripts/reinforcement_learn.sh
+++ scripts/reinforcement_learn.sh
@@ -4,4 +4,3 @@ git diff >>git_commit_id.txt
 echo -e "reinforcementLearn\nquit\n" | ./Miacis_*
 zip -rq learn_kifu.zip learn_kifu
 rm -rf learn_kifu
-scp -r $(pwd) sakoda:~/learn_result/reinforcement/
--- scripts/supervised_learn.sh
+++ scripts/supervised_learn.sh
@@ -2,4 +2,3 @@ git show -s > git_commit_id.txt
 git diff   >> git_commit_id.txt
 ~/Miacis/scripts/generate_torch_script_model.py
 echo -e "supervisedLearn\nquit\n" | ./Miacis_*
-scp -r `pwd` sakoda:~/learn_result/supervised/
