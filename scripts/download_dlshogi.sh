ee9d181f3d0a38a1b66d2addbf5a3500378885c0
sakoda <sakoda@keio.jp>
2021-05-22 07:31:22 +0000

    dlshogiのモデルをダウンロードするスクリプトを追加

---
+++ scripts/download_dlshogi.sh
@@ -0,0 +1,3 @@
+mkdir dlshogi_onnx_model && cd dlshogi_onnx_model
+wget https://github.com/TadaoYamaoka/DeepLearningShogi/releases/download/wcwc31/dlshogi_with_gct_wcsc31.zip
+unzip dlshogi_with_gct_wcsc31.zip
