acf48209ac263911b3c2eef9492224fc4934db84
sakoda <sakoda@keio.jp>
2021-08-28 19:15:21 +0000

    download_libtorch.shを更新

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -3,12 +3,13 @@ cd $(dirname $0)
 # どこに保存するかの基準位置
 root_dir=../..
 
-version=1.8.1
-url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2Bcu102.zip
-file_name=libtorch-cxx11-abi-shared-with-deps-${version}+cu102.zip
+version=1.9.0
+cuda_version=cu111
+url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2B${cuda_version}.zip
+file_name=libtorch-cxx11-abi-shared-with-deps-${version}+${cuda_version}.zip
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/${url_file_name}
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/${cuda_version}/${url_file_name}
 unzip -q ${root_dir}/${file_name} -d ${root_dir}/libtorch-tmp
-mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}
+mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}-${cuda_version}
 rm ${root_dir}/${file_name}
 rmdir ${root_dir}/libtorch-tmp
