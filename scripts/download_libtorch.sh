164d87bd5716b90907ac20468e5d95191efcca33
sakoda <sakoda@keio.jp>
2021-11-21 10:50:06 +0900

    基本バージョン設定を変更

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -1,13 +1,14 @@
+set -eu
 cd $(dirname $0)
 
 # どこに保存するかの基準位置
 root_dir=../..
 
-version=1.8.1
-url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2Bcu102.zip
-file_name=libtorch-cxx11-abi-shared-with-deps-${version}+cu102.zip
+version=1.10.0
+url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2Bcu113.zip
+file_name=libtorch-cxx11-abi-shared-with-deps-${version}+cu113.zip
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/${url_file_name}
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu113/${url_file_name}
 unzip -q ${root_dir}/${file_name} -d ${root_dir}/libtorch-tmp
 mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}
 rm ${root_dir}/${file_name}
