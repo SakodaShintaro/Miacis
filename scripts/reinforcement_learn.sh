57f993d923b1070706b582153550a1c1501c2994
sakoda <sakoda@keio.jp>
2021-12-28 21:39:44 +0900

    強化学習スクリプト修正

--- scripts/reinforcement_learn.sh
+++ scripts/reinforcement_learn.sh
@@ -1,6 +1,3 @@
 git show -s >git_commit_id.txt
 git diff >>git_commit_id.txt
-~/Miacis/scripts/generate_torch_script_model.py
-echo -e "reinforcementLearn\nquit\n" | ./Miacis_*
-zip -rq learn_kifu.zip learn_kifu
-rm -rf learn_kifu
+echo -e "reinforcementLearn\nquit\n" | ./Miacis_shogi_categorical
