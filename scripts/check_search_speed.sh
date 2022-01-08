93b1395d0a689fb190309dba1e803e08f3af689e
sakoda <sakoda@keio.jp>
2022-01-08 12:09:54 +0900

    コメントアウト失敗していたミスを修正

--- scripts/check_search_speed.sh
+++ scripts/check_search_speed.sh
@@ -8,10 +8,10 @@ go"
 echo ${command} | ./Miacis_shogi*
 
 
-```
+<< COMMENTOUT
 コピペ用
 checkSearchSpeed
 setoption name USI_Hash value 8192
 setoption name model_name value shogi_cat_bl10_ch256.engine
 go
-```
+COMMENTOUT
