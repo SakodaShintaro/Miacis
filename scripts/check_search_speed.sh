0db2e26bad37fa326f1e21b018ca4134a0e99190
sakoda <sakoda@keio.jp>
2022-01-08 12:06:42 +0900

    コピペ用コメント追加

--- scripts/check_search_speed.sh
+++ scripts/check_search_speed.sh
@@ -6,3 +6,12 @@ setoption name model_name value ${1}
 go"
 
 echo ${command} | ./Miacis_shogi*
+
+
+```
+コピペ用
+checkSearchSpeed
+setoption name USI_Hash value 8192
+setoption name model_name value shogi_cat_bl10_ch256.engine
+go
+```
