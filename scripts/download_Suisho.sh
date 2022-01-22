c74816f73000a38fa039c465a656bf4b64ddf42d
Shintaro Sakoda <sakoda@keio.jp>
2022-01-22 10:30:14 +0000

    水匠5ダウンロードスクリプト修正

--- scripts/download_Suisho.sh
+++ scripts/download_Suisho.sh
@@ -1,37 +1,38 @@
+set -eux
+
 cd `dirname $0`
 
 # どこに保存するかの基準位置
 root_dir=../..
 
+# ディレクトリ作成
+mkdir ${root_dir}/Suisho
+
+# 移動
+cd ${root_dir}/Suisho
+
+# 評価関数取得
+curl -RLo ./suisho5_20211123.halfkp.nnue.cpp.xz https://github.com/mizar/YaneuraOu/releases/download/resource/suisho5_20211123.halfkp.nnue.cpp.xz
+
 #
 # やねうら王のダウンロード & コンパイル
 #
 # GitHubからソースコードをダウンロード
-git clone https://github.com/yaneurao/YaneuraOu ${root_dir}/Suisho
-cd ${root_dir}/Suisho/source
-git checkout b0a3a2a4f7565bbefb85999368df15e9c90c621f
+git clone https://github.com/yaneurao/YaneuraOu
 
-# デフォルトではclangを使うようになっているがg++を使いたいのでMakefileを書き換える
-sed -i -e "s/#COMPILER = g++/COMPILER = g++/g" Makefile
-sed -i -e "s/COMPILER = clang++/#COMPILER = clang++/g" Makefile
+# ビルド
+cd ./YaneuraOu/source &&\
+    xz -cdk ../..//suisho5_20211123.halfkp.nnue.cpp.xz > eval/nnue/embedded_nnue.cpp &&\
+    nice make -j$(nproc) YANEURAOU_EDITION=YANEURAOU_ENGINE_NNUE TARGET_CPU=AVX2 EVAL_EMBEDDING=ON EXTRA_CPPFLAGS='-DENGINE_OPTIONS="\"option=name=FV_SCALE=type=spin=default=24=min=1=max=128\""' COMPILER=g++ TARGET=./Suisho5-YaneuraOu-tournament-avx2 tournament >& >(tee ./Suisho5-YaneuraOu-tournament-avx2.log)
 
-# コンパイル
-make -j4
+# 元の位置に戻る
+cd ../../
 
-# binディレクトリを作ってそこに動くような環境を作る
-mkdir ../bin
-cd ../bin
-mv ../source/YaneuraOu-by-gcc .
-
-# Suisho評価関数のダウンロード
-mkdir eval
-cd eval
-wget https://pjvyqa.bn.files.1drv.com/y4m_dJvlgaZbQFN72YJYe1gQg_hkvKNGYkZjfaGCuVYBKXdFVVD8_sf1TyRk6-ln0waU94nC60BKlfsdMjTcx_sZ7cFC_0594k0-osbqAypam4DGwRN69DghRddCEwq-F5uWzphDLg_u4uxhXeRIL1ZBh1BzAnleilMPOP9I2-XF69-CRYjFG0Mf_FJ9xwWHv4INtldTpuyvtq2BaLIxgGLdw -O Suisho.zip
-unzip -q Suisho.zip
-mv РЕПа2/eval/nn.bin ./ # 文字化けしてる
+# バイナリを持ってくる
+mv ./YaneuraOu/source/Suisho5-YaneuraOu-tournament-avx2 .
 
 # standard_book.dbのダウンロード
-mkdir ../book
-cd ../book
+mkdir ./book
+cd ./book
 wget https://github.com/yaneurao/YaneuraOu/releases/download/v4.73_book/standard_book.zip
 unzip -q standard_book.zip
