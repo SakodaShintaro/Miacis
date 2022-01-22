bbdd86c3b618d6f12ef494d1feb5b019a0cf43c0
Shintaro Sakoda <sakoda@keio.jp>
2022-01-22 06:21:11 +0000

    valid_tsv解析スクリプトを追加

---
+++ scripts/parse_valid_loss.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import argparse
+import pandas as pd
+from collections import defaultdict
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("tsv_file1", type=str)
+parser.add_argument("tsv_file2", type=str)
+args = parser.parse_args()
+
+label1 = "ResNet"
+label2 = "ViT"
+
+df1 = pd.read_csv(args.tsv_file1, sep="\t", header=None)
+df2 = pd.read_csv(args.tsv_file2, sep="\t", header=None)
+
+#########
+# 4分類 #
+#########
+# grid_result = defaultdict(int)
+
+# for i in tqdm(range(len(df1))):
+#     sfen1, move_str1, move_prob1, value_label1, policy_loss1, value_loss1 = df1.iloc[i]
+#     sfen2, move_str2, move_prob2, value_label2, policy_loss2, value_loss2 = df2.iloc[i]
+
+#     assert sfen1 == sfen2
+#     grid_result[(policy_loss1 > policy_loss2, value_loss1 > value_loss2)] += 1
+
+# print(grid_result)
+
+##########
+# 手数毎 #
+##########
+INTERVAL = 10
+
+turn_policy_loss1 = defaultdict(float)
+turn_policy_loss2 = defaultdict(float)
+turn_value_loss1 = defaultdict(float)
+turn_value_loss2 = defaultdict(float)
+turn_num = defaultdict(int)
+
+for i in tqdm(range(len(df1))):
+    sfen1, move_str1, move_prob1, value_label1, policy_loss1, value_loss1 = df1.iloc[i]
+    sfen2, move_str2, move_prob2, value_label2, policy_loss2, value_loss2 = df2.iloc[i]
+
+    turn_number = int(sfen1.split(" ")[-1])
+    turn_number = turn_number // INTERVAL * INTERVAL
+
+    turn_policy_loss1[turn_number] += policy_loss1
+    turn_policy_loss2[turn_number] += policy_loss2
+    turn_value_loss1[turn_number] += value_loss1
+    turn_value_loss2[turn_number] += value_loss2
+    turn_num[turn_number] += 1
+
+
+def sorted_dict(d):
+    return dict(sorted(d.items(), key=lambda x: x[0]))
+
+
+turn_policy_loss1 = sorted_dict(turn_policy_loss1)
+turn_policy_loss2 = sorted_dict(turn_policy_loss2)
+turn_value_loss1 = sorted_dict(turn_value_loss1)
+turn_value_loss2 = sorted_dict(turn_value_loss2)
+turn_num = sorted_dict(turn_num)
+
+for key, val in turn_num.items():
+    turn_policy_loss1[key] /= val
+    turn_policy_loss2[key] /= val
+    turn_value_loss1[key] /= val
+    turn_value_loss2[key] /= val
+
+print(turn_policy_loss1)
+print(turn_policy_loss2)
+
+plt.plot(turn_policy_loss1.keys(), turn_policy_loss1.values(), label=label1)
+plt.plot(turn_policy_loss2.keys(), turn_policy_loss2.values(), label=label2)
+plt.xlabel("手数")
+plt.ylabel("Policy損失")
+plt.legend()
+plt.savefig("turn_policy_loss.png", bbox_inches="tight", pad_inches=0.05)
+plt.cla()
+
+plt.plot(turn_value_loss1.keys(), turn_value_loss1.values(), label=label1)
+plt.plot(turn_value_loss2.keys(), turn_value_loss2.values(), label=label2)
+plt.xlabel("手数")
+plt.ylabel("Value損失")
+plt.legend()
+plt.savefig("turn_value_loss.png", bbox_inches="tight", pad_inches=0.05)
+plt.cla()
