ab7d8b9fdf4d5c6603401c3ff1c787e39ed17e4e
sakoda <sakoda@keio.jp>
2022-03-05 20:45:44 +0900

    cu113を利用することで3090でも動作確認

--- scripts/download_libtorch.sh
+++ scripts/download_libtorch.sh
@@ -4,11 +4,12 @@ cd $(dirname $0)
 root_dir=../..
 
 version=1.10.1
-url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2Bcu102.zip
-file_name=libtorch-cxx11-abi-shared-with-deps-${version}+cu102.zip
+CUDA_VERSION=113
+url_file_name=libtorch-cxx11-abi-shared-with-deps-${version}%2Bcu${CUDA_VERSION}.zip
+file_name=libtorch-cxx11-abi-shared-with-deps-${version}+cu${CUDA_VERSION}.zip
 
-wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu102/${url_file_name}
+wget -P ${root_dir}/ https://download.pytorch.org/libtorch/cu${CUDA_VERSION}/${url_file_name}
 unzip -q ${root_dir}/${file_name} -d ${root_dir}/libtorch-tmp
-mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}
+mv ${root_dir}/libtorch-tmp/libtorch ${root_dir}/libtorch-${version}-cu${CUDA_VERSION}
 rm ${root_dir}/${file_name}
 rmdir ${root_dir}/libtorch-tmp
