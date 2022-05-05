b93e5383a4f6db48e9e656af6f348e3781e623a1
sakoda <sakoda@keio.jp>
2022-05-05 14:42:14 +0900

    model_dictを追加

--- python/train.py
+++ python/train.py
@@ -6,6 +6,7 @@ import argparse
 from common import seconds_to_pretty_str
 from scheduler import LinearWarmupAndCooldownScheduler
 import time
+from model_dict import model_dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--learning_rate", type=float, default=0.01)
@@ -17,7 +18,7 @@ parser.add_argument("--save_interval", type=int, default=1600001)
 parser.add_argument("--learn_rate_decay_mode", type=int, default=0)
 parser.add_argument("--learn_rate_decay_period", type=int, default=1600000)
 parser.add_argument("--clip_grad_norm", type=float, default=10)
-parser.add_argument("--model_name", type=str, default=None)
+parser.add_argument("--model_name", type=str, default="resnet")
 parser.add_argument("--break_near_24h", action="store_true")
 args = parser.parse_args()
 
@@ -40,7 +41,10 @@ block_num = 20
 channel_num = 512
 policy_channel_num = 27
 
-model = CategoricalNetwork(INPUT_CHANNEL_NUM, block_num, channel_num, policy_channel_num, BOARD_SIZE)
+model_class = model_dict[args.model_name]
+
+model = model_class(INPUT_CHANNEL_NUM, block_num, channel_num, policy_channel_num, BOARD_SIZE)
+torch.save(model.state_dict(), f"{args.model_name}_bl{block_num}_ch{channel_num}_before_learn.pt")
 
 trainset = MiacisDataSet('/home/sakoda/data/ShogiAIBookData/dlshogi_with_gct-001.hcpe')
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True)
@@ -89,5 +93,4 @@ for step, batch in enumerate(trainloader):
     optim.step()
     scheduler.step()
 
-
-torch.save(model.state_dict(), "model.pt")
+torch.save(model.state_dict(), f"{args.model_name}_bl{block_num}_ch{channel_num}.pt")
