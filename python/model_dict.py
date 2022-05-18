963fff4b89dd482fecbccfb3402417952e1ccb5c
sakoda <sakoda@keio.jp>
2022-05-18 22:26:41 +0900

    normformer追加

--- python/model_dict.py
+++ python/model_dict.py
@@ -2,10 +2,12 @@ from model.resnet import CategoricalNetwork
 from model.vit import TransformerModel
 from model.timm_vit import TimmVit
 from model.deepnet import DeepNet
+from model.normformer import NormFormer
 
 model_dict = {
     "resnet": CategoricalNetwork,
     "vit": TransformerModel,
     "timm_vit": TimmVit,
     "deepnet": DeepNet,
+    "normformer": NormFormer,
 }
