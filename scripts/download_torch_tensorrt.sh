0491e86060fec419a28bbe761419cf332858208e
Shintaro Sakoda <sakoda@keio.jp>
2022-07-18 12:34:31 +0000

    削除対象を間違えていたミスを修正

--- scripts/download_torch_tensorrt.sh
+++ scripts/download_torch_tensorrt.sh
@@ -10,4 +10,4 @@ FILE_URL=https://github.com/pytorch/TensorRT/releases/download/v1.1.0/${FILE_NAM
 
 wget ${FILE_URL}
 tar xfv libtorchtrt-v1.1.0-cudnn8.2-tensorrt8.2-cuda11.3-libtorch1.11.0.tar.gz 
-rm ${FILE_URL}
+rm ${FILE_NAME}
