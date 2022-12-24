301523e9231524b8d44542b723f84efde8722922
Shintaro Sakoda <sakoda@keio.jp>
2022-12-24 17:09:07 +0900

    テストスクリプト更新

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,18 @@
 set -eux
 
-SCRIPT_DIR=$(dirname $0)
+# ライブラリパスへの設定をちゃんとしないと動かない
+LD_LIBRARY_PATH=/root/libtorch-1.11.0/lib/
 
+# ビルドディレクトリへ移動
+SCRIPT_DIR=$(dirname $0)
 cd ${SCRIPT_DIR}/../build/
 
+# Make
 make -j$(nproc) Miacis_shogi_categorical
 
+# テスト実行
 command="usi
 setoption name model_name value shogi_cat_bl10_ch256.ts
 isready
 "
-
 echo ${command} | ./Miacis_shogi_categorical
