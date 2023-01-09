f2a133eb9bbb7bfa918e2e8df445c1618fb132b4
Shintaro Sakoda <sakoda@keio.jp>
2023-01-09 07:05:40 +0000

    torch_tensorrtの方にもバージョン名を入れるように修正

--- scripts/download_torch_tensorrt.sh
+++ scripts/download_torch_tensorrt.sh
@@ -5,8 +5,10 @@ cd $(dirname $0)
 # このスクリプトから見て2つ上に保存する
 cd ../..
 
-FILE_NAME=libtorchtrt-1.2.0-cudnn8.4-tensorrt8.4-cuda11.6-libtorch1.12.1-x86_64-linux.tar.gz
-FILE_URL=https://github.com/pytorch/TensorRT/releases/download/v1.2.0/${FILE_NAME}
+VERSION=1.2.0
+FILE_NAME=libtorchtrt-${VERSION}-cudnn8.4-tensorrt8.4-cuda11.6-libtorch1.12.1-x86_64-linux.tar.gz
+FILE_URL=https://github.com/pytorch/TensorRT/releases/download/v${VERSION}/${FILE_NAME}
 wget ${FILE_URL}
 tar xfv ${FILE_NAME}
 rm ${FILE_NAME}
+mv torch_tensorrt torch_tensorrt-${VERSION}
